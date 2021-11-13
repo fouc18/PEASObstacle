b06489922b89ae5b7a45c0dfb1f98d6aac260034
Gabriel Côté <cotg18@uqo.ca>
2021-11-13 18:48:41 -0500

    Ajout Classe Fourmis

---
+++ Fourmis.py
@@ -0,0 +1,39 @@
+class Fourmi:
+    def __init__(self):
+
+        self.posX 
+        self.posY
+        pass
+
+    '''
+    E/: Chemin de s a t
+    V: Nombre de sommet du graphe  
+    G: Graphe (V, E/) avec comme sommet initial s
+    a: defini l'influence des pheromones sur le choix du prochain chemin
+    s: point initial
+    t: point final
+    '''
+    def Initialize(self,G, s):
+        
+
+        pass
+
+    def SelectNextEdge(self, i, k):
+        pass
+    def ComputeCoefficient(self,i,j,k):
+        pass
+    def EvaporateOfShortestPath(self,i,j):
+        pass
+
+    def getPos(self):
+
+        return [self.posX, self.posY] 
+
+    def setPos(self, posX, posY):
+        self.posX = posX
+        self.posY = posY
+
+
+    def move(self):
+        pass
+        
